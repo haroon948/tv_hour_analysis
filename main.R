ef74fc1140b6323eefce4a3c978f5adea21d65d8
Haruna Aremu <35647056+haroon948@users.noreply.github.com>
2025-04-08 10:45:45 +0100

    filtered to ages 30 and above

--- main.R
+++ main.R
@@ -2,10 +2,15 @@ pacman::p_load(tidyverse, here)
 
 tv_watched <- gss_cat %>% 
   group_by(marital) %>% 
+  filter(age>=30) %>% 
   summarise(mean_tv = mean(tvhours, na.rm= T)) %>% 
   ungroup()
 
-ggplot(data = tv_watched,
+tv_watched_plot <- ggplot(data = tv_watched,
        aes(x= mean_tv,
            fill = marital))+
   geom_histogram()
+
+write_csv(tv_watched, here("tvHoursByMarital.csv"))
+
+# write_csv(tv_watched_plot, here("tvWatchedPlot.pdf"))
